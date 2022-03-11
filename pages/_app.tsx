8d17bdcc7f1cb61f80432686f2031cdf7ac62d67
titsex <61911688+titsex@users.noreply.github.com>
2022-03-11 19:15:51 +0300

    🔥Minor fixes #6 &⚡Updating dependencies
    Updated the dependencies, now it seems the assembly will be even easier, because of typescript 4.6. More details: https://devblogs.microsoft.com/typescript/announcing-typescript-4-6/#no-void-0-react-jsx.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,14 +15,15 @@ const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
             <Head>
                 <title>Next Extensive Template</title>
 
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="manifest" href="/site.webmanifest" />
                 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
 
                 <meta charSet="UTF-8" />
 
+                <meta name="msapplication-TileColor" content="#333333" />
                 <meta name="theme-color" content="#ffffff" />
                 <meta name="robots" content="index,follow" />
                 <meta name="googlebot" content="index,follow" />
